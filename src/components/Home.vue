a01fd62510f2633dbf87e9febb45abd981701eb7
haowj <git_email_address>
2020-04-20 21:31:29 +0800

    update

---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <el-button type="info" @click="logout">退出</el-button>
+    Home组件
+  </div>
+</template>
+
+<script>
+  export default {
+    methods: {
+      logout() {
+        this.$router.push('/login')
+        window.sessionStorage.clear()
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
-
+// 导入全局样式表
+import './assets/css/global.css'
+import './assets/font-awesome-4.7.0/css/font-awesome.min.css'
+import axios from 'axios'
+axios.defaults.baseURL = 'http://127.0.0.1:8099/'
+Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 new Vue({
